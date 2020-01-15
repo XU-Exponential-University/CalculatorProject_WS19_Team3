642280c85ded3916f690e73c8b3ea9b1a0ff7749
Benjamin Homuth <bhomuth99@gmail.com>
2020-01-15 11:17:37 +0100

    Initial Commit

---
+++ SwiftUI - Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI - Calculator
+//
+//  Created by Benjamin Homuth on 15.01.20.
+//  Copyright © 2020 Benjamin Homuth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
