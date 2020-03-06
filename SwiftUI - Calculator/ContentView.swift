4ca70758e1a5833a94b63a206d8e06d21666bb8c
DrOpossum <gerberding.felix@gmail.com>
2020-03-06 12:05:30 +0100

    added comments and fixed regex

--- SwiftUI - Calculator/ContentView.swift
+++ SwiftUI - Calculator/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+// MARK: Felix Part I
+
 extension String {
     func matches(_ regex: String) -> Bool {
         return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
@@ -16,41 +18,49 @@ extension String {
 
 extension NSExpression {
 
+    // Converts everything in a NSExpression to doubles so a decimal result is returned.
     func toFloatingPoint() -> NSExpression {
+        // Switch trough different types of expressions that need different handling
         switch expressionType {
-        case .constantValue:
-            if let value = constantValue as? NSNumber {
-                return NSExpression(forConstantValue: NSNumber(value: value.doubleValue))
+            
+            // Constant values
+            case .constantValue:
+                if let value = constantValue as? NSNumber {
+                    return NSExpression(forConstantValue: NSNumber(value: value.doubleValue))
+                }
+            
+            //
+            case .function:
+               let newArgs = arguments.map { $0.map { $0.toFloatingPoint() } }
+               return NSExpression(forFunction: operand, selectorName: function, arguments: newArgs)
+            case .conditional:
+               return NSExpression(forConditional: predicate, trueExpression: self.true.toFloatingPoint(), falseExpression: self.false.toFloatingPoint())
+            case .unionSet:
+                return NSExpression(forUnionSet: left.toFloatingPoint(), with: right.toFloatingPoint())
+            case .intersectSet:
+                return NSExpression(forIntersectSet: left.toFloatingPoint(), with: right.toFloatingPoint())
+            case .minusSet:
+                return NSExpression(forMinusSet: left.toFloatingPoint(), with: right.toFloatingPoint())
+            case .subquery:
+                if let subQuery = collection as? NSExpression {
+                    return NSExpression(forSubquery: subQuery.toFloatingPoint(), usingIteratorVariable: variable, predicate: predicate)
+                }
+            case .aggregate:
+                if let subExpressions = collection as? [NSExpression] {
+                    return NSExpression(forAggregate: subExpressions.map { $0.toFloatingPoint() })
+                }
+            case .anyKey:
+                fatalError("anyKey not yet implemented")
+            case .block:
+                fatalError("block not yet implemented")
+            case .evaluatedObject, .variable, .keyPath:
+                break // Nothing to do here
             }
-        case .function:
-           let newArgs = arguments.map { $0.map { $0.toFloatingPoint() } }
-           return NSExpression(forFunction: operand, selectorName: function, arguments: newArgs)
-        case .conditional:
-           return NSExpression(forConditional: predicate, trueExpression: self.true.toFloatingPoint(), falseExpression: self.false.toFloatingPoint())
-        case .unionSet:
-            return NSExpression(forUnionSet: left.toFloatingPoint(), with: right.toFloatingPoint())
-        case .intersectSet:
-            return NSExpression(forIntersectSet: left.toFloatingPoint(), with: right.toFloatingPoint())
-        case .minusSet:
-            return NSExpression(forMinusSet: left.toFloatingPoint(), with: right.toFloatingPoint())
-        case .subquery:
-            if let subQuery = collection as? NSExpression {
-                return NSExpression(forSubquery: subQuery.toFloatingPoint(), usingIteratorVariable: variable, predicate: predicate)
-            }
-        case .aggregate:
-            if let subExpressions = collection as? [NSExpression] {
-                return NSExpression(forAggregate: subExpressions.map { $0.toFloatingPoint() })
-            }
-        case .anyKey:
-            fatalError("anyKey not yet implemented")
-        case .block:
-            fatalError("block not yet implemented")
-        case .evaluatedObject, .variable, .keyPath:
-            break // Nothing to do here
-        }
-        return self
+            return self
     }
 }
+
+// MARK: Benny's Code I
 
 struct ContentView: View {
     
@@ -61,7 +71,6 @@ struct ContentView: View {
     @State private var isTypingNumber = false
     @State private var calcHistory: [String: Double] = [:]
     
-    // MARK: --------- Body ---------
     // basically the whole body (=everything u see on screen)
     var body: some View {
         VStack(spacing: 20) {
@@ -113,9 +122,9 @@ struct ContentView: View {
                     }
                     
                     Button(action: {}) {
-                        Text("%")
+                        Text("+/-")
                     }.onTapGesture {
-                        self.operandTapped("%")
+                        self.plusMinusToggle()
                     }
                     
                 }.buttonStyle(calcButton())
@@ -218,17 +227,26 @@ struct ContentView: View {
         
     }
     
-    // MARK: --------- Calculations ---------
-    // pressing a number
+    // MARK: Felix Code II
+    
+    // Fired when a digit is tappen. Appends the digit to the calculation or replaced it when it is empty
     private func digitTapped(_ number: String) -> Void {
-        if(calculatorText == "0"){
+        if(calculatorText == "0" || calculatorText == "Error"){
             calculatorText = number;
         } else {
             calculatorText.append(number);
         }
     }
     
-    // basic calculations
+    private func plusMinusToggle(){
+        if(calculatorText.hasPrefix("-")){
+            calculatorText = String(calculatorText.dropFirst(1))
+        } else {
+            calculatorText = "-\(calculatorText)"
+        }
+    }
+    
+    // adds operands to the calculation and formats them accordingly
     private func operandTapped(_ operand: String) {
         
         var formattedOperand = "";
@@ -250,60 +268,74 @@ struct ContentView: View {
 
     }
     
-    func checkParentheses(s: String) -> Int {
-        let pairs: [Character: Character] = ["(": ")", "[": "]", "{": "}"]
+    func checkParentheses(s: String) -> [Character] {
+        let pairs: [Character: Character] = ["(": ")", "[": "]", "{": "}"] // Those are the pairs of brackets we are looking for
         var stack: [Character] = []
-        for char in s {
+        for char in s { // Loop through the equation
             if let match = pairs[char] {
-                stack.append(match)
+                stack.append(match) // We found a bracket and are now looking for the closing one
             } else if stack.last == char {
-                stack.popLast()
+                stack.popLast() // Ignore the warning Xcode gives you, doing this is just faster then manually removing the last element
             }
         }
-        return stack.count
+        return stack // In the end we return the unclosed brackets
     }
     
+    func properRound(number: Double) -> Double {
+        return Double(round(10000 * number)/10000)
+    }
     
+    // returns a boolean if a string matches a given regex
     func matchesForRegexInText(regex: String!, text: String!) -> Bool {
 
+        // Do & Catch to pevent issues with invalid regex syntax (that might be caused through later changes)
         do {
             let regex = try NSRegularExpression(pattern: regex, options: [])
             let nsString = text as NSString
-            guard let result = regex.firstMatch(in: text, options: [], range: NSMakeRange(0, nsString.length)) else {
+            
+            guard regex.firstMatch(in: text, options: [], range: NSMakeRange(0, nsString.length)) != nil else {
                 return false // pattern does not match the string
             }
-            return true
+            
+            return true // pattern does match (because a first match was found)
         } catch let error as NSError {
             print("invalid regex: \(error.localizedDescription)")
             return false
         }
+        
     }
     
-    // what happens when you press "="
+    // Fired when you press the equal sign. First validating and sanitizing the input that is then passed onto NSExpressions to evaluate
     private func calculate() {
-        let inputExpression = calculatorText;
-        var cleanedExpression = inputExpression.replacingOccurrences(of: "√", with: "sqrt")
         
-        if matchesForRegexInText(regex: "^([0-9\\-\\*\\+\\s\\(\\)\\.,]+)$", text: cleanedExpression) {
+        let inputExpression = calculatorText;
+        
+        // Check if input is fa valid formula. Regex is explained in the documentation
+        if !matchesForRegexInText(regex: "^([0-9\\-\\*\\+\\s\\(\\)\\.,√\\/]+)$", text: inputExpression) || !matchesForRegexInText(regex: "^[(]?[-]?([0-9√]+)[)(]??([)(]?([-+/*]?[0-9]?[)(]?([0-9]))?([.,][0-9]+)?[)]?)*$", text: inputExpression) {
             calculatorText = "Error"
         } else {
             
+            // Replace signs used for better visuals with commands required by NSExpressions
+            var cleanedExpression = inputExpression.replacingOccurrences(of: "√", with: "sqrt")
+
+            // Find the amout of missing Parentheses
             let missingParentheses = checkParentheses(s: cleanedExpression)
-            print(missingParentheses)
-            if(missingParentheses > 0){
-                for _ in 1...missingParentheses {
-                    cleanedExpression = cleanedExpression + ")"
+            // and add the missing amount
+            if(missingParentheses.count > 0){
+                for i in missingParentheses {
+                    cleanedExpression = cleanedExpression + "\(i)"
                 }
             }
             
+            // create Expression as Float
             let expr = NSExpression(format: cleanedExpression).toFloatingPoint()
             do {
-                
+                // Get result and round it
                 let res:Double = try! expr.expressionValue(with: nil, context: nil) as! Double
-                calculatorText = String(res)
+                calculatorText = String(properRound(number: res))
                 calcHistory[inputExpression] = res
             } catch {
-                calculatorText = "Error"
+                calculatorText = "ExError"
             }
             
         }
@@ -311,7 +343,7 @@ struct ContentView: View {
     }
     
     
-    // MARK: --------- ButtonStyle ---------
+    // MARK: Benny's Code II
    
     // Button style for all Numbers
     struct numberButton: PrimitiveButtonStyle {
@@ -362,10 +394,7 @@ struct ContentView: View {
         }
     }
     
-    
 
-    
-    // MARK: --------- ContenView Preview ---------
     // This is just for preview reasons here, do not delete or change (unless you know what you are doing)!!
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
